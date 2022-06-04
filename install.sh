967c2536d8393fb7529b85ff5d3f7c4f437d3036
birx-web <74237969+birx-web@users.noreply.github.com>
2022-06-05 01:41:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt -y update && sudo apt -y upgrade
 sudo apt -y install lsb-release apt-transport-https ca-certificates
 sudo apt install -y build-essential apache2 mariadb-server mariadb-client bison flex php php-curl php-cli php-common php-mysql php-pear php-gd php-mbstring php-intl php-ldap php-bcmath php-xml curl sox libncurses5-dev libssl-dev mpg123 libxml2-dev libnewt-dev sqlite3 libsqlite3-dev pkg-config automake libtool autoconf git unixodbc-dev uuid uuid-dev libasound2-dev libogg-dev libvorbis-dev libicu-dev libcurl4-openssl-dev libical-dev libneon27-dev libsrtp0-dev libspandsp-dev sudo subversion libtool-bin python-dev unixodbc dirmngr asterisk asterisk-config asterisk-core-sounds-ru asterisk-core-sounds-ru-g722 asterisk-core-sounds-ru-gsm asterisk-core-sounds-ru-wav asterisk-modules asterisk-mp3 asterisk-mysql asterisk-ooh323 asterisk-opus asterisk-voicemail nodejs
+sudo apt-get install nodejs-dev node-gyp libssl1.0-dev
 
 sudo systemctl restart asterisk
 sudo systemctl disable asterisk
