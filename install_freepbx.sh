21c331fc45b6a8d598f216f72d70937999b69070
birx-web <74237969+birx-web@users.noreply.github.com>
2022-06-05 00:21:21 +0300

    Update install_freepbx.sh

--- install_freepbx.sh
+++ install_freepbx.sh
@@ -1,10 +1,6 @@
-yum -y update
-yum -y groupinstall core base "Development Tools"
-yum -y install lynx mariadb-server mariadb php php-mysql php-mbstring tftp-server httpd ncurses-devel sendmail sendmail-cf sox newt-devel libxml2-devel libtiff-devel audiofile-devel gtk2-devel subversion kernel-devel git php-process crontabs cronie cronie-anacron wget vim php-xml uuid-devel sqlite-devel net-tools gnutls-devel php-pear
+apt -y update
+apt -y install mariadb-server mariadb php php-mysql php-mbstring tftp-server httpd ncurses-devel sendmail sendmail-cf sox newt-devel libxml2-devel libtiff-devel audiofile-devel gtk2-devel subversion kernel-devel git php-process crontabs cronie cronie-anacron php-xml uuid-devel sqlite-devel net-tools gnutls-devel php-pear
 pear install Console_Getopt
-
-firewall-cmd --zone=public --add-port=80/tcp --permanent
-firewall-cmd --reload
 
 # Enable MariaDB
 systemctl enable mariadb.service
@@ -24,13 +20,13 @@ cd /usr/src
 wget http://downloads.asterisk.org/pub/telephony/dahdi-linux-complete/dahdi-linux-complete-current.tar.gz
 wget http://downloads.asterisk.org/pub/telephony/libpri/libpri-current.tar.gz
 wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz
-wget -O jansson.tar.gz https://github.com/akheron/jansson/archive/v2.7.tar.gz
-wget http://www.pjsip.org/release/2.4/pjproject-2.4.tar.bz2
+wget -O jansson.tar.gz https://github.com/akheron/jansson/archive/v2.14.tar.gz
+wget -O pjsip.tar.gz https://github.com/pjsip/pjproject/archive/refs/tags/2.12.1.tar.gz
 
 cd /usr/src
-tar -xjvf pjproject-2.4.tar.bz2
-rm -f pjproject-2.4.tar.bz2
-cd pjproject-2.4
+tar -vxfz pjsip.tar.bz2
+rm -f pjsip.tar.bz2
+cd pjsip
 CFLAGS='-DPJ_HAS_IPV6=1' ./configure --prefix=/usr --enable-shared --disable-sound\
   --disable-resample --disable-video --disable-opencore-amr --libdir=/usr/lib64
 make dep
@@ -48,8 +44,8 @@ make install
 
 # Install Asterisk
 cd /usr/src
-tar xvfz asterisk-13-current.tar.gz
-rm -f asterisk-13-current.tar.gz
+tar xvfz asterisk-19-current.tar.gz
+rm -f asterisk-19-current.tar.gz
 cd asterisk-*
 contrib/scripts/install_prereq install
 ./configure --libdir=/usr/lib64
@@ -92,9 +88,9 @@ systemctl restart httpd.service
 
 # Install FreePBX
 cd /usr/src
-wget http://mirror.freepbx.org/modules/packages/freepbx/freepbx-13.0-latest.tgz
-tar xfz freepbx-13.0-latest.tgz
-rm -f freepbx-13.0-latest.tgz
+wget http://mirror.freepbx.org/modules/packages/freepbx/freepbx-15.0-latest.tgz
+tar xfz freepbx-15.0-latest.tgz
+rm -f freepbx-15.0-latest.tgz
 cd freepbx
 ./start_asterisk start
 ./install -n
